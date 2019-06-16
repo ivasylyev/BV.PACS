211f58f9772e98cfcdc822975dbd37ae93af8bae
Ivan <VasylyevI@bv.com>
2019-06-16 12:02:03 +0200

    refactoring

---
+++ BV.PACS.Client/Shared/Catalogs/CatalogComponent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using BV.PACS.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BV.PACS.Client.Shared.Catalog
+{
+    public class CatalogComponent : ComponentBase
+    {
+        protected CatalogState CatalogState { get; } = new CatalogState();
+
+        public int ActivePageNumber
+        {
+            get => CatalogState.Condition.PageNumber;
+            set
+            {
+                CatalogState.Condition.PageNumber = value;
+                BeginGetDataAsync(CatalogState.Condition).ContinueWith(x => { StateHasChanged(); });
+            }
+        }
+
+        protected override async Task OnInitAsync()
+        {
+            await BeginGetDataAsync(CatalogState.Condition);
+            await BeginGetPageCountAsync(CatalogState.Condition);
+        }
+
+
+        protected void OnSearchPanelToggle()
+        {
+            CatalogState.SearchPanelToggle();
+
+            DoSearch();
+        }
+
+        protected void OnSearchPanelSearch(AggregatedConditionDto cond)
+        {
+            CatalogState.SetSearchPanelCondition(cond);
+
+            DoSearch();
+        }
+
+        protected void DoSearch()
+        {
+            BeginGetDataAsync(CatalogState.Condition).ContinueWith(x => { StateHasChanged(); });
+            BeginGetPageCountAsync(CatalogState.Condition).ContinueWith(x => { StateHasChanged(); });
+        }
+
+        protected virtual async Task BeginGetPageCountAsync(AggregatedConditionDto cond)
+        {
+            await Task.Run(() => throw new NotImplementedException("Must be overriden in the child class"));
+        }
+
+        protected virtual async Task BeginGetDataAsync(AggregatedConditionDto cond)
+        {
+            await Task.Run(() => throw new NotImplementedException("Must be overriden in the child class"));
+        }
+    }
+}
---
+++ BV.PACS.Client/Shared/Catalogs/CatalogState.cs
@@ -0,0 +1,30 @@
+﻿using BV.PACS.Shared.Models;
+
+namespace BV.PACS.Client.Shared.Catalog
+{
+    public class CatalogState
+    {
+        public const int CollapseButtonCount = 10;
+
+        private readonly AggregatedConditionDto _condition = new AggregatedConditionDto();
+        private AggregatedConditionDto _lastSearchPanelCondition = new AggregatedConditionDto();
+        public AggregatedConditionDto Condition => SearchPanelCollapsed ? _condition : _lastSearchPanelCondition;
+
+        private bool SearchPanelCollapsed { get; set; } = true;
+
+        public string SearchPanelCssClass => SearchPanelCollapsed ? "collapse" : "col-sm-3";
+        public string GridPanelCssClass => SearchPanelCollapsed ? "col-sm-12" : "col-sm-9";
+
+
+        public void SetSearchPanelCondition(AggregatedConditionDto cond)
+        {
+            _lastSearchPanelCondition = cond;
+        }
+        public void SearchPanelToggle()
+        {
+            SearchPanelCollapsed = !SearchPanelCollapsed;
+
+           
+        }
+    }
+}
