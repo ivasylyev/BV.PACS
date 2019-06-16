a53f28e76a47aa85bdf9c0b4f7835182043e9515
Ivan <VasylyevI@bv.com>
2019-06-16 12:23:51 +0200

    refactor material catalog

--- BV.PACS.Client/Shared/Catalogs/CatalogState.cs
+++ BV.PACS.Client/Shared/Catalogs/CatalogState.cs
@@ -6,25 +6,25 @@ namespace BV.PACS.Client.Shared.Catalog
     {
         public const int CollapseButtonCount = 10;
 
-        private readonly AggregatedConditionDto _condition = new AggregatedConditionDto();
-        private AggregatedConditionDto _lastSearchPanelCondition = new AggregatedConditionDto();
         public AggregatedConditionDto Condition => SearchPanelCollapsed ? _condition : _lastSearchPanelCondition;
 
-        private bool SearchPanelCollapsed { get; set; } = true;
+        private readonly AggregatedConditionDto _condition = new AggregatedConditionDto();
+        private AggregatedConditionDto _lastSearchPanelCondition = new AggregatedConditionDto();
 
         public string SearchPanelCssClass => SearchPanelCollapsed ? "collapse" : "col-sm-3";
         public string GridPanelCssClass => SearchPanelCollapsed ? "col-sm-12" : "col-sm-9";
+        public string ShowHideSearchPanelCaption => SearchPanelCollapsed ?  "Show Search Panel": "Hide Search Panel";
+        private bool SearchPanelCollapsed { get; set; } = true;
 
 
         public void SetSearchPanelCondition(AggregatedConditionDto cond)
         {
             _lastSearchPanelCondition = cond;
         }
+
         public void SearchPanelToggle()
         {
             SearchPanelCollapsed = !SearchPanelCollapsed;
-
-           
         }
     }
 }
