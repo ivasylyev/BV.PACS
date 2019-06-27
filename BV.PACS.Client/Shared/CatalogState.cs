722ed5ea0ab07b54730a49abb8222fad7f6a496f
Ivan <VasylyevI@bv.com>
2019-06-27 22:36:46 +0200

    add some styles

--- BV.PACS.Client/Shared/CatalogState.cs
+++ BV.PACS.Client/Shared/CatalogState.cs
@@ -11,8 +11,8 @@ namespace BV.PACS.Client.Shared
         private readonly AggregatedConditionDto _condition = new AggregatedConditionDto();
         private AggregatedConditionDto _lastSearchPanelCondition = new AggregatedConditionDto();
 
-        public string SearchPanelCssClass => SearchPanelCollapsed ? "collapse" : "col-sm-3";
-        public string GridPanelCssClass => SearchPanelCollapsed ? "col-sm-12" : "col-sm-9";
+        public string SearchPanelCssClass => SearchPanelCollapsed ? "collapse" : "col-sm-3 alert alert-primary background-white";
+        public string GridPanelCssClass => SearchPanelCollapsed ? "col-sm-12 alert alert-primary background-white" : "col-sm-9 alert alert-primary background-white";
         public string ShowHideSearchPanelCaption => SearchPanelCollapsed ? "Show Search Panel" : "Hide Search Panel";
         private bool SearchPanelCollapsed { get; set; } = true;
 
