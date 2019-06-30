041cf33e7d6d63c96cca80ec67f083b69b85e0dc
Ivan <VasylyevI@bv.com>
2019-06-30 21:56:19 +0200

    add material tracking dto

---
+++ BV.PACS.Client/Pages/Materials/MaterialTrackingPanelCode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using BV.PACS.Shared.Models;
+
+namespace BV.PACS.Client.Materials
+{
+    public class MaterialTrackingPanelCode: BV.PACS.Client.Shared.Base.TrackingPanel<BV.PACS.Shared.Models.MaterialTrackingDto>
+    {
+        protected TemplateLookupItem Template
+        {
+            get { return Templates.FirstOrDefault(t => t.Id == TrackingObject.MaterialTemplateId); }
+            set
+            {
+                TrackingObject.MaterialTemplateId = value.Id;
+                TrackingObject.MaterialTemplateName = value.Name;
+            }
+        }
+
+        protected string MaterialBarcode
+        {
+            get => TrackingObject.MaterialBarcode;
+            set => TrackingObject.MaterialBarcode = value;
+        }
+
+
+        protected string SourceBarcode
+        {
+            get => TrackingObject.SourceBarcode;
+            set => TrackingObject.SourceBarcode = value;
+        }
+
+        protected string SourceType
+        {
+            get => "Not implemented";
+            set { }
+        }
+
+
+        protected string MaterialType
+        {
+            get => TrackingObject.MaterialType;
+            set => TrackingObject.MaterialType = value;
+        }
+
+        protected string MaterialOwnerName
+        {
+            get => TrackingObject.MaterialOwnerName;
+            set => TrackingObject.MaterialOwnerName = value;
+        }
+
+        protected string ParentMaterialBarcode
+        {
+            get => TrackingObject.ParentMaterialBarcode;
+            set => TrackingObject.ParentMaterialBarcode = value;
+        }
+
+
+        protected DateTime MaterialRegistrationDate
+        {
+            get => TrackingObject.MaterialRegistrationDate;
+            set => TrackingObject.MaterialRegistrationDate = value;
+        }
+
+        protected string MaterialNote
+        {
+            get => TrackingObject.MaterialNote;
+            set => TrackingObject.MaterialNote = value;
+        }
+
+        protected string MaterialPointOfOrigin
+        {
+            get => TrackingObject.MaterialPointOfOrigin;
+            set => TrackingObject.MaterialPointOfOrigin = value;
+        }
+    }
+}
--- BV.PACS.Shared/Models/TrackingForms/MaterialTrackingDto.cs
+++ BV.PACS.Shared/Models/TrackingForms/MaterialTrackingDto.cs
@@ -1,8 +1,73 @@
-﻿namespace BV.PACS.Shared.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BV.PACS.Shared.Models
 {
     [DataUrl("api/Tracking/GetMaterial")]
     public class MaterialTrackingDto
     {
-        
+
+        [Column("idfMaterial")]
+        public int MaterialId { get; set; }
+
+        [Column("idfsCFormTemplateID")]
+        public string MaterialTemplateId { get; set; }
+
+        [Column("strBarcode")]
+        public string MaterialBarcode { get; set; }
+
+        [Column("strNote")]
+        public string MaterialNote { get; set; }
+
+        [Column("idfsMaterialTypeID")]
+        public string MaterialTypeId { get; set; }
+
+        [Column("MaterialType")]
+        public string MaterialType { get; set; }
+
+
+        [Column("TemplateName")]
+        public string MaterialTemplateName { get; set; }
+
+        [Column("idfOwner")]
+        public int MaterialOwnerId { get; set; }
+
+        [Column("Owner")]
+        public string MaterialOwnerName { get; set; }
+
+        [Column("idfSource")]
+        public int SourceId { get; set; }
+
+        [Column("datRegistration_Date")]
+        public DateTime MaterialRegistrationDate { get; set; }
+
+        [Column("ParentMaterialNumber")]
+        public string ParentMaterialBarcode { get; set; }
+
+        [Column("ParentMaterial")]
+        public int ParentMaterialId { get; set; }
+
+        [Column("SourceBarcode")]
+        public string SourceBarcode { get; set; }
+
+        [Column("Location")]
+        public string MaterialPointOfOrigin { get; set; }
+
+        [Column("Isolation_idfGeoLocation")]
+        public int AddressGeoLocationId { get; set; }
+
+        [Column("idfsGeoLocation")]
+        public int GeoLocationId { get; set; }
+
+        [Column("idfsGeoLocationNew")]
+        public int GeoLocationNewId { get; set; }
+
+        [Column("strCultureShortName")]
+        public string MaterialTypeShort { get; set; }
+       
+        public override string ToString()
+        {
+            return $"Id:'{MaterialBarcode}', Registered:'{MaterialRegistrationDate}', Template:'{MaterialTemplateName}'";
+        }
     }
 }
