6694c73b32e433819f66fa81a6961ca0c4d6df81
Ivan <VasylyevI@bv.com>
2019-07-01 21:33:36 +0200

    add source and aliquot dto

--- BV.PACS.Shared/Models/TrackingForms/AliquotTrackingDto.cs
+++ BV.PACS.Shared/Models/TrackingForms/AliquotTrackingDto.cs
@@ -1,8 +1,129 @@
-﻿namespace BV.PACS.Shared.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BV.PACS.Shared.Models
 {
     [DataUrl("api/Tracking/GetAliquot")]
     public class AliquotTrackingDto
     {
-        
+        [Column("idfContainer")]
+        public int AliquotId { get; set; }
+
+        [Column("idfActivity")]
+        public int ActivityId { get; set; }
+
+        [Column("strBarcode")]
+        public string AliquotBarcode { get; set; }
+
+        [Column("datCreationDate")]
+        public DateTime AliquotCreationDate { get; set; }
+
+        [Column("strNote")]
+        public string AliquotNote { get; set; }
+
+        [Column("fltVolume")]
+        public float Volume { get; set; }
+
+        [Column("fltWeight")]
+        public float Weight { get; set; }
+
+        [Column("NullableVolume")]
+        public float? NullableVolume { get; set; }
+
+        [Column("NullableWeight")]
+        public float? NullableWeight { get; set; }
+
+        [Column("intGeneration")]
+        public int Generation { get; set; }
+
+        [Column("StatusRef_Name")]
+        public string AliquotStatus { get; set; }
+
+        [Column("idfsMaterialType")]
+        public string MaterialTypeId { get; set; }
+
+        [Column("MaterialType")]
+        public string MaterialType { get; set; }
+
+        [Column("strCultureShortName")]
+        public string MaterialTypeShort { get; set; }
+
+        [Column("LocationPath")]
+        public string LocationPath { get; set; }
+
+        [Column("idfMaterial")]
+        public int MaterialId { get; set; }
+
+        [Column("idfSubdivisionID")]
+        public int SubdivisionId { get; set; }
+
+        [Column("idfsContainer_Status")]
+        public string AliquotStatusId { get; set; }
+
+        [Column("idfsStoring_Type")]
+        public string StoringTypeId { get; set; }
+
+        [Column("idfsVolumeUnit")]
+        public string VolumeUnitId { get; set; }
+
+        [Column("idfsWeightUnit")]
+        public string WeightUnitId { get; set; }
+
+        [Column("idfsWeightUnit")]
+        public string AliquotInitialBarcode { get; set; }
+
+        [Column("strInitialContainerID")]
+        public string InitialContainerId { get; set; }
+
+        [Column("Material_strBarcode")]
+        public string MaterialBarcode { get; set; }
+
+        [Column("VolumeUnitName")]
+        public string VolumeUnitName { get; set; }
+
+        [Column("WeightUnitName")]
+        public string WeightUnitName { get; set; }
+
+        [Column("Container_Detail_Status")]
+        public string AliquotDetailStatus { get; set; }
+
+        [Column("blnTrackStockContainers")]
+        public bool TrackStocAliquots { get; set; }
+
+        [Column("idfParentActivity")]
+        public int ParentActivityId { get; set; }
+
+        [Column("idfsActivity_Type")]
+        public string ActivityType { get; set; }
+
+        [Column("idfsDerivativeType")]
+        public string DerivativeTypeId { get; set; }
+
+        [Column("DerivativeType")]
+        public string DerivativeType { get; set; }
+
+        [Column("strDerivativeShortName")]
+        public string DerivativeShortName { get; set; }
+
+        [Column("TemplateName")]
+        public string AliquotTemplateName { get; set; }
+
+        [Column("idfsCFormTemplateID")]
+        public string AliquotTemplateId { get; set; }
+
+        [Column("strParentBarcode")]
+        public string AliquotParentBarcode { get; set; }
+
+        [Column("idfsUsageType")]
+        public string UsageTypeId { get; set; }
+
+        [Column("UsageType_strDefault")]
+        public string UsageType { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id:'{AliquotBarcode}', Created:'{AliquotCreationDate}', Template:'{AliquotTemplateName}'";
+        }
     }
 }
--- BV.PACS.Shared/Models/TrackingForms/TestTrackingDto.cs
+++ BV.PACS.Shared/Models/TrackingForms/TestTrackingDto.cs
@@ -1,8 +1,83 @@
-﻿namespace BV.PACS.Shared.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BV.PACS.Shared.Models
 {
     [DataUrl("api/Tracking/GetTest")]
     public class TestTrackingDto
     {
-        
+        [Column("idfTest")]
+        public int TestId { get; set; }
+
+        [Column("idfsTestType")]
+        public string TestTypeId { get; set; }
+
+        [Column("datTestDate")]
+        public DateTime TestDate { get; set; }
+
+        [Column("idfsTestResult")]
+        public string TestResultId { get; set; }
+
+        [Column("idfsCFormTemplateID")]
+        public string TestTemplateId { get; set; }
+
+        [Column("TemplateName")]
+        public string TestTemplateName { get; set; }
+
+        [Column("strBarcode")]
+        public string TestBarcode { get; set; }
+
+        [Column("strNote")]
+        public string TestNote { get; set; }
+
+        [Column("datRegistration_Date")]
+        public DateTime TestRegistrationDate { get; set; }
+
+        [Column("idfsTestStatus")]
+        public string TestStatusId { get; set; }
+
+        [Column("idfContainer")]
+        public string AliquotId { get; set; }
+
+
+        [Column("strSourceBarcode")]
+        public string SourceBarcode { get; set; }
+
+        [Column("strContainerBarcode")]
+        public string AliquotBarcode { get; set; }
+
+
+        [Column("idfsMaterialType")]
+        public string MaterialTypeId { get; set; }
+
+
+        [Column("TestType")]
+        public string TestType { get; set; }
+
+        [Column("TestResult")]
+        public string TestResult { get; set; }
+
+        [Column("TestStatus")]
+        public string TestStatus { get; set; }
+
+
+        [Column("idfsTestSet")]
+        public string TestSetId { get; set; }
+
+        [Column("idfSource")]
+        public string SourceId { get; set; }
+
+
+        [Column("strMaterialBarcode")]
+        public string MaterialBarcode { get; set; }
+
+        [Column("idfMaterial")]
+        public string MaterialId { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id:'{TestBarcode}', Registered:'{TestRegistrationDate}', Template:'{TestTemplateName}'";
+        }
     }
 }
