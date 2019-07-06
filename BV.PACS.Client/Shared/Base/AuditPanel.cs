b89b537a4fcd2988e428aa03edff27fdcfbd9ad8
Ivan <VasylyevI@bv.com>
2019-07-06 17:40:32 +0200

    working on audit panels

---
+++ BV.PACS.Client/Shared/Base/AuditPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BV.PACS.Shared.Models;
+
+namespace BV.PACS.Client.Shared.Base
+{
+    public class AuditPanel<T> : GridPanel<T> where T : AuditGridDto, new()
+    {
+        protected Dictionary<string, AuditGroupedDto<T>> GroupedSource { get; set; }
+
+        public void OnClick(string groupName)
+        {
+            GroupedSource[groupName].GroupVisibility = !GroupedSource[groupName].GroupVisibility;
+            StateHasChanged();
+        }
+
+        protected override async Task OnInitAsync()
+        {
+            await base.OnInitAsync();
+            GroupedSource = new Dictionary<string, AuditGroupedDto<T>>();
+
+            foreach (var dto in DataSource)
+            {
+                if (!GroupedSource.ContainsKey(dto.GroupHeader))
+                {
+                    GroupedSource.Add(dto.GroupHeader, new AuditGroupedDto<T>(dto.GroupHeader));
+                }
+                GroupedSource[dto.GroupHeader].Data.Add(dto);
+            }
+        }
+    }
+}
