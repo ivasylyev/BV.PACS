5cec2bad584f5a4d456e19aacb0faaf230919fe9
Ivan <Ivan@GRADIENT-PC>
2019-07-08 17:22:49 +0200

    refactoring

--- BV.PACS.Client/i18ntext/@types/BV.PACS.Client.I18nText.Text.cs
+++ BV.PACS.Client/i18ntext/@types/BV.PACS.Client.I18nText.Text.cs
@@ -2,7 +2,7 @@ namespace BV.PACS.Client.I18nText
 {
     public partial class Text : global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage, global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextLateBinding
     {
-        string global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage.FallBackLanguage => "en";
+        string global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage.FallBackLanguage => "en-US";
 
         public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
