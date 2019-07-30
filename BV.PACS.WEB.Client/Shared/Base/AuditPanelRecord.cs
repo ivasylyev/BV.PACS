28aee206e4d140af419fb2b1e73f92171deb3fc7
Ivan <Ivan@GRADIENT-PC>
2019-07-30 20:15:31 +0200

    created base class for audit panel record

---
+++ BV.PACS.WEB.Client/Shared/Base/AuditPanelRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using BV.PACS.WEB.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor.I18nText.Interfaces;
+
+namespace BV.PACS.WEB.Client.Shared.Base
+{
+    public class AuditPanelRecord<TModel, TTranslation> : GridPanel<TModel, TTranslation>
+        where TModel : AuditGridDto, new()
+        where TTranslation : class, I18nTextFallbackLanguage, new()
+    {
+        [Parameter]
+        public Action OnClick { get; set; }
+
+        [Parameter]
+        public KeyValuePair<string, AuditGroupedDto<TModel>> AuditData { get; set; }
+
+        public void DoClick()
+        {
+            AuditData.Value.GroupVisibility = !AuditData.Value.GroupVisibility;
+            StateHasChanged();
+            OnClick.Invoke();
+        }
+    }
+}
