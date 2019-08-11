64d6f5ac82d28aafe7127c8ce504db45dccad169
Ivan <Ivan@GRADIENT-PC>
2019-08-11 23:09:30 +0200

    add header for BMR

--- BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.BatchContentPanel.cs
+++ BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.BatchContentPanel.cs
@@ -6,5 +6,25 @@ namespace BV.PACS.WEB.Client.I18nText
 
         public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
+        /// <summary>"Aliquots"</summary>
+        public string Aliquots;
+
+        /// <summary>"Aliquot Template"</summary>
+        public string AliquotTemplate;
+
+        /// <summary>"Generate"</summary>
+        public string Generate;
+
+        /// <summary>"Materials"</summary>
+        public string Materials;
+
+        /// <summary>"Material Template"</summary>
+        public string MaterialTemplate;
+
+        /// <summary>"Sources"</summary>
+        public string Sources;
+
+        /// <summary>"Source Template"</summary>
+        public string SourceTemplate;
     }
 }
