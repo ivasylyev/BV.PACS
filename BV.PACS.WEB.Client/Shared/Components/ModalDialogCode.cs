05710e4f40d5bfc775541f846db71bff6538b8d7
Ivan <Ivan@GRADIENT-PC>
2019-08-16 21:32:12 +0200

    refactoring dialog add edit template to BMR

---
+++ BV.PACS.WEB.Client/Shared/Components/ModalDialogCode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace BV.PACS.WEB.Client.Shared.Components
+{
+    public class ModalDialogCode: ComponentBase
+    {
+
+        [Parameter]
+        public Action OnCancel { get; set; }
+        [Parameter]
+        public string CancelText { get; set; }
+
+        [Parameter]
+        public Action OnOk { get; set; }
+
+        [Parameter]
+        public string OkText { get; set; } 
+
+        [Parameter]
+        public bool Show { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected void Cancel()
+        {
+            Show = false;
+            OnCancel?.Invoke();
+
+            StateHasChanged();
+        }
+
+        protected void Ok()
+        {
+            Show = false;
+            OnOk?.Invoke();
+
+            StateHasChanged();
+        }
+    }
+}
