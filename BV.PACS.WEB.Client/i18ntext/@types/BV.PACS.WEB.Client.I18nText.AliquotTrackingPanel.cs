1f531b8ebc484f64d94543bd83997d299daf4356
Ivan <VasylyevI@bv.com>
2019-08-19 10:02:58 +0200

    aDD mtf AND atf TRANSLATIONS

--- BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.AliquotTrackingPanel.cs
+++ BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.AliquotTrackingPanel.cs
@@ -6,13 +6,52 @@ namespace BV.PACS.WEB.Client.I18nText
 
         public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
+        /// <summary>"Aliquot ID"</summary>
+        public string AliquotBarcode;
+
+        /// <summary>"Creation Date"</summary>
+        public string AliquotCreationDate;
+
+        /// <summary>"Notes"</summary>
+        public string AliquotNote;
+
+        /// <summary>"Parent Barcode"</summary>
+        public string AliquotParentBarcode;
+
+        /// <summary>"Aliquot Status"</summary>
+        public string AliquotStatus;
+
+        /// <summary>"Template"</summary>
+        public string AliquotTemplateName;
+
         /// <summary>"Derivative Type"</summary>
         public string DerivativeType;
+
+        /// <summary>"Location Path"</summary>
+        public string LocationPath;
+
+        /// <summary>"Material ID"</summary>
+        public string MaterialBarcode;
 
         /// <summary>"Material Type"</summary>
         public string MaterialType;
 
+        /// <summary>"Passage"</summary>
+        public string Passage;
+
         /// <summary>"Source Type"</summary>
         public string SourceType;
+
+        /// <summary>"Volume"</summary>
+        public string Volume;
+
+        /// <summary>"Volume Unit"</summary>
+        public string VolumeUnitName;
+
+        /// <summary>"Weight"</summary>
+        public string Weight;
+
+        /// <summary>"Weight Unit"</summary>
+        public string WeightUnitName;
     }
 }
--- BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.MaterialTrackingPanel.cs
+++ BV.PACS.WEB.Client/i18ntext/@types/BV.PACS.WEB.Client.I18nText.MaterialTrackingPanel.cs
@@ -6,8 +6,32 @@ namespace BV.PACS.WEB.Client.I18nText
 
         public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
+        /// <summary>"Material ID"</summary>
+        public string MaterialBarcode;
+
+        /// <summary>"Notes"</summary>
+        public string MaterialNote;
+
+        /// <summary>"Materail Owner"</summary>
+        public string MaterialOwnerName;
+
+        /// <summary>"Point Of Origin"</summary>
+        public string MaterialPointOfOrigin;
+
+        /// <summary>"Creation Date"</summary>
+        public string MaterialRegistrationDate;
+
+        /// <summary>"Template"</summary>
+        public string MaterialTemplateName;
+
         /// <summary>"Material Type"</summary>
         public string MaterialType;
+
+        /// <summary>"Parent Material ID"</summary>
+        public string ParentMaterialBarcode;
+
+        /// <summary>"Source ID"</summary>
+        public string SourceBarcode;
 
         /// <summary>"Source Type"</summary>
         public string SourceType;
